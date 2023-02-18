4d02fe164ca72bfdaf8ab406d44af766eaeca7c3
Muhammad Sulaiman <trendingofyt@gmail.com>
2023-02-18 22:21:49 +0500

    responsive added

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -8,18 +8,19 @@ const Dashboard = () => {
     <>
       <Header noBooking={noBooking} type="dashboard"/>
       <div className='p-5'>
-      {
+     
+        <div className='bg-white p-5'>
+          <div className='w-full text-[#434343] border-b border-[#F0F0F0] pb-4 mb-6'>田中太郎さん　の予約状況</div>
+          {
         noBooking === true
         ?
-        <div className='bg-white p-5'>
-          <div className='w-full text-[#434343] border-b border-[#F0F0F0] pb-4'>田中太郎さん　の予約状況</div>
           <div className='text-[#F5222D] mt-6'>現在仮登録なため、ご予約ができません。<br/>オーナーが許可する次第、正式に登録が完了いたします。</div>
+          :
+          <div>
+          asd
+          </div>}
         </div>
-        :
-        <div className='bg-white p-5'>
-
-        </div>
-      }
+      
       </div>
     </>
   )
