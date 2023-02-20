a71abcafcbd75430d7ed84718984f8ac105ab6f3
Muhammad Sulaiman <trendingofyt@gmail.com>
2023-02-20 22:15:48 +0500

    alright

--- src/user-pages/Account-Managment.js
+++ src/user-pages/Account-Managment.js
@@ -2,14 +2,15 @@ import {useState} from 'react'
 import Header from '../user-components/Header'
 
 const AccountManagment = () => {
-
+     const [tab, settab] = useState(1)
   return (
     <>
       <Header type="account-managment"/>
-        <div className='p-5'>
-        <div className='bg-white p-5'>
-         account managment
-      </div>
+        <div className='bg-white p-5 pb-0'>
+         <div className='text-[30px] font-bold'>アカウント管理</div>
+         <div className="flex gap-5 mt-6">
+            <button className={`${tab === 1 ? "text-[#2F54EB] border-b-2 border-[#2F54EB] pb-2" : "text-[#434343]"}`}>ご契約内容</button>
+         </div>
       </div>
     </>
   )
